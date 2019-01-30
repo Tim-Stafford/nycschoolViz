3f2d1dfe8565c26e6ef39b6b9995968831b4fc88
tim-stafford <tim@boboscarhappyapple.com>
2019-01-30 13:53:59 -0500

    added educator quality guide

--- src/main.js
+++ src/main.js
@@ -1,87 +1,51 @@
 var data;
 var projection;
-var w = 1000, h = 800;
+var w = 1000;
 var mymap = L.map('mapid').setView([40.7, -73.90], 11);
- 
+
 function initializeData(csv) {
-  data = csv.map(function(d) {
-    return {
-      lat: +d.lat,
-      lon: +d.lon,
-      school: d.LOCATION_NAME,
-      schoolType: d.schoolType,
-      economicNeedIndex: +d.economicNeedIndex
-    }
-  });
-}
-
-
-
- 
-function initProjection() {
-  var geoJSON = {
-    type: "FeatureCollection",
-    features: []
-  }
- 
-  data.forEach(function(d) {
-    geoJSON.features.push({
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [d.lon, d.lat]
-      }
+    data = csv.map(function(d) {
+        return {
+            lat: +d.lat,
+            lon: +d.lon,
+            LOCATION_NAME: d.LOCATION_NAME,
+            schoolType: d.schoolType,
+            economicNeedIndex: +d.economicNeedIndex
+        }
     });
-  });
- 
-  projection = d3.geoMercator();
-  projection.fitSize([w, h], geoJSON);
 }
 
 
 function addLayers() {
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiYm9ib3NjYXJoYXBweWFwcGxlIiwiYSI6ImNqcmZnemx1NzFzeXQ0M3FuMHRqMDYwaHMifQ.yturWl0gYhIdGprffSEJSw'
-}).addTo(mymap);
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiYm9ib3NjYXJoYXBweWFwcGxlIiwiYSI6ImNqcmZnemx1NzFzeXQ0M3FuMHRqMDYwaHMifQ.yturWl0gYhIdGprffSEJSw'
+    }).addTo(mymap);
 }
 
 
 function addSchools() {
     data.forEach(
-        function(d) {
+    function(d) {
             if (d.schoolType == "Charter") {
-            L.circle([d.lat, d.lon], {
-            color: '#F7910B',
-            radius: 100
-            }).addTo(mymap);
-            }   else {
+                L.circle([d.lat, d.lon], {
+                    color: '#F7910B',
+                    radius: 100
+                }).bindPopup(d.LOCATION_NAME).addTo(mymap)
+            } else {
                 L.circle([d.lat, d.lon], {
                     color: '#6699CC'
-                    }).addTo(mymap);
-
-     }
+                }).bindPopup(d.LOCATION_NAME).addTo(mymap);
+            }
         })
 }
-
-
-
-
-
-
-
-
-
 
 
 d3.csv('data/schoolslatlon.csv', function(err, csv) {
     initializeData(csv);
     addLayers();
-    initProjection();
     addSchools();
-    
-  });
 
-
+});
